1d1149a800386f314600b8715aec8ef296f4d7b3
Myles Inglis <mi219@kent.ac.uk>
2016-06-07 15:21:33 +0100

    Created EventType Enum.

---
+++ include/Event.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Myles Inglis on 07/06/2016.
+//
+
+#ifndef CHIPAUDIO_EVENT_HPP
+#define CHIPAUDIO_EVENT_HPP
+
+enum EventType
+{
+    NOTE_ON,
+    NOTE_OFF,
+    PARAM_CHANGE
+};
+
+#endif //CHIPAUDIO_EVENT_HPP
