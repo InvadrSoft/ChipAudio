37bd5abb0d4d6532942c842b6cead3efaa1e701b
Myles Inglis <mi219@kent.ac.uk>
2016-06-07 16:11:56 +0100

    Doxygen comments added

--- include/TimeSignature.hpp
+++ include/TimeSignature.hpp
@@ -9,9 +9,17 @@ namespace chip
 {
     struct TimeSignature
     {
+        /**
+         * Default constructor.
+         */
         TimeSignature()
                 : beats(4), noteValue(4) {}
 
+        /**
+         * Non-default constructor.
+         * @param _beats The number of beats in each bar
+         * @param _noteValue The value of one beat
+         */
         TimeSignature(unsigned int _beats, unsigned int _noteValue)
                 : beats(_beats), noteValue(_noteValue) {}
 
