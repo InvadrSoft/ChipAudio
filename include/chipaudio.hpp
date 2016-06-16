96a1a97b6a4592ace890bdf7f46ada198449ff61
Myles Inglis <mi219@kent.ac.uk>
2016-06-16 10:27:33 +0100

    Added chipaudio and chipmodules headers

---
+++ include/chipaudio.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Myles Inglis on 08/12/2015.
+//
+
+#ifndef CHIPAUDIO_CHIPAUDIO_H
+#define CHIPAUDIO_CHIPAUDIO_H
+
+#include "Chip.hpp"
+
+#endif //CHIPAUDIO_CHIPAUDIO_H
---
+++ include/chipmodules.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Myles Inglis on 16/06/2016.
+//
+
+#ifndef CHIPAUDIO_CHIPMODULES_HPP
+#define CHIPAUDIO_CHIPMODULES_HPP
+
+#include "Add.hpp"
+#include "EnvelopeADSR.hpp"
+#include "FilterLP1Pole.hpp"
+#include "Invert.hpp"
+#include "Multiply.hpp"
+#include "NoteToFrequency.hpp"
+#include "Subtract.hpp"
+
+#endif //CHIPAUDIO_CHIPMODULES_HPP
